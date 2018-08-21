af0551781f02a61f2f439c1a5f13fd8253d4342d
LithiumDe <32262175+LithiumDe@users.noreply.github.com>
2018-08-21 22:35:42 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
 <?php
-spl_autoload_register(function($class_name){
-include "engine/".$class_name.".class.php";
-});
-
-
-$digit1 = new digital('Book',100,10);
-$digit1->render();
-
-$real = new Physical('Fruits', 50, 5);
-$real->render();
-
-$weight = new Weight('Vegetables', 25, 30);
-$weight->render();
-?>
-<html lang="en">
-        <head>
-            <title>HW2</title>
-            <link rel="stylesheet" href="css/style.css">
-        </head>
-        <body>
-            
-        </body>
-</html>
+class Product
+{
+    private $id;
+    private $name;
+    private $price;
+    private $quantity;
+    
+    function __construct($id, $name, $price, $quantity)
+    {
+        $this->id= $id;
+        $this->name = $name;
+        $this->price = $price;
+        $this->quantity = $quantity;
+    }
+    public function view()
+    {
+        echo "<p>$this->id</p><p>$this->name</p><p>$this->price</p><p>$this->quantity</p>";
+    }
+}
+$product = new Product(1,"Product", 1000, 100);
+$product->view();
